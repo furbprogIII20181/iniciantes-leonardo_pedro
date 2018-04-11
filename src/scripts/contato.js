f2b7481d6cf36ed2ffcfa141eca963ff5d68bc89
Leonardo Fronza <lfronza312@gmail.com>
2018-04-11 19:22:50 -0300

    email

---
+++ src/scripts/contato.js
@@ -0,0 +1,12 @@
+function enviaEmail(){
+    console.log("entrou");
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               console.log(this.responseText);
+            }
+        };
+        xmlhttp.open("GET", "email.php", true);
+        xmlhttp.send();
+    }
+}
