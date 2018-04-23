3c59413c6a3f39c43699dbb57b5e81dc7f5e1282
Leonardo Fronza <lfronza312@gmail.com>
2018-04-23 19:30:15 -0300

    Melhorias no email

--- src/scripts/email.php
+++ src/scripts/email.php
@@ -1,21 +1,25 @@
-<?php
-//Variáveis
 
+<?php 
+$nome  = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$msg = $_POST['msg'];
 
-// emails para quem será enviado o formulário
 $emailenviar = "leofronza@furb.br";
 $destino = $emailenviar;
-$assunto = "Contato pelo Site";
+//$assunto = "Contato pelo Site: ";
+//$msg = <?php echo $_GET["message"]; 
 
-// É necessário indicar que o formato do e-mail é html
+
 $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From: $nome <$email>';
-//$headers .= "Bcc: $EmailPadrao\r\n";
+    $headers .= 'From: '+ $email;
 
-ini_set();
 
-$enviaremail = mail($destino, $assunto, "OI" , $headers);
+
+
+
+$enviaremail = mail($destino, $assunto, $msg , $headers);
 $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
 echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
 $mgm = "ERRO AO ENVIAR E-MAIL!";
