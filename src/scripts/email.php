0e4901f7562bd6525c9ec8534ae73a4ba675e8a7
Pedro Alba <pedrohalba@hotmail.com>
2018-04-23 19:40:26 -0300

    Ajustes email

--- src/scripts/email.php
+++ src/scripts/email.php
@@ -7,16 +7,8 @@ $msg = $_POST['msg'];
 
 $emailenviar = "leofronza@furb.br";
 $destino = $emailenviar;
-//$assunto = "Contato pelo Site: ";
-//$msg = <?php echo $_GET["message"]; 
 
-
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From: '+ $email;
-
-
-$enviaremail = mail($destino, $assunto, $msg , $headers);
+$enviaremail = mail($destino, $assunto, $msg);
 $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
 echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
 $mgm = "ERRO AO ENVIAR E-MAIL!";
