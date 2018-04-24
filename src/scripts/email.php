ce116370323cdcec86e86d5868d06ec82934a77c
Leonardo Fronza <lfronza312@gmail.com>
2018-04-24 15:05:09 -0300

    Correção no email

--- src/scripts/email.php
+++ src/scripts/email.php
@@ -3,15 +3,21 @@
 $nome  = $_POST['nome'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
-$msg = $_POST['msg']. " \nEnviado por: ". $nome . ' <'. $email . '>';
+$msg = $_POST['msg']. " \n\nEnviado por: ". $nome . ' <'. $email . '>';
 
-$msg = $msg + "Enviado de " + $nome + "<" + $email + ">";
+if ($nome != null or $email != null){
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From:'. $nome .'<'.$email.'>';
+
+//$msg = $msg + "Enviado de " + $nome + "<" + $email + ">";
 $emailenviar = "leofronza@furb.br";
 $destino = $emailenviar;
 
 $enviaremail = mail($destino, $assunto, $msg);
 
 echo " <meta http-equiv='refresh' content='1;URL=../pages/contato.html'>";
-echo "<script>alert('Email enviado com Sucesso!);</script>";
 
+}
 ?>
